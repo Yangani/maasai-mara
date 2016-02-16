e619c6681fa4a875f6705fc022526d6b83b2a08e
Festus Kiprop <fyangani@gmail.com>
2016-02-16 11:06:57 -0600

    Functionality for non-touch devices

--- build/script.js
+++ build/script.js
@@ -31,4 +31,26 @@
    return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
   }
 
+  //For non touch devices
+  if (!is_touch_device()) {
+      var exHandler = function(ev){
+        imghero.style.WebkitTransform = 'perspective(1000px) translate3d(0,0,0) rotate3d(1,1,1,0deg)';
+        imghero.style.transform = 'perspective(1000px) translate3d(0,0,0) rotate3d(1,1,1,0deg)';
+      };
+
+      var moveHandler = throttle(function(ev) {
+          var xVal = -1/(win.height/2)*ev.clientY + 1,
+            yVal = 1/(win.width/2)*ev.clientX - 1,
+            transX = 20/(win.width)*ev.clientX - 10,
+            transY = 20/(win.height)*ev.clientY - 10,
+            transZ = 100/(win.height)*ev.clientY - 50;
+            imghero.style.WebkitTransform = 'perspective(1000px) translate3d(' + transX + 'px,' + transY + 'px,' + transZ + 'px) rotate3d(' + xVal + ',' + yVal + ',0,2deg)';
+            imghero.style.transform = 'perspective(1000px) translate3d(' + transX + 'px,' + transY + 'px,' + transZ + 'px) rotate3d(' + xVal + ',' + yVal + ',0,2deg)';
+        }, 100);
+
+      header.addEventListener('mouseenter', exHandler);
+      window.addEventListener('mousemove', moveHandler);
+      header.addEventListener('mouseleave', exHandler);
+  }
+
 }());
