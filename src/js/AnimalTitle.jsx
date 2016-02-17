bb508ea36ab10e0b9e820bbdf80ce9ba69de3e86
Festus Kiprop <fyangani@gmail.com>
2016-02-17 10:31:40 -0600

    Added animals title script for <html>

---
+++ src/js/AnimalTitle.jsx
@@ -0,0 +1,11 @@
+  var AnimalTitle = React.createClass({
+    render: function(){
+      return (<div className='title sm-col sm-col-6'>
+        <div className="sci-name">{this.props.sciName}</div>
+        <h1>{this.props.comName}</h1>
+        </div>
+        ) ;
+    }
+  });
+
+module.exports = AnimalTitle;
