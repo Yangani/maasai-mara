6e5e4e6dd3d3393760b2acd917d0a23eec772e25
Festus Kiprop <fyangani@gmail.com>
2016-02-29 22:12:13 -0600

    Handling onclick events for animal wrapper

--- src/js/AnimalWrapper.jsx
+++ src/js/AnimalWrapper.jsx
@@ -3,6 +3,9 @@
   var AnimalImage = require('./AnimalImage.jsx');
 
   var AnimalWrapper = React.createClass({
+    handleClick: function () {
+      this.props.onClick(this);
+    },
   });
 
 module.exports = AnimalWrapper;
