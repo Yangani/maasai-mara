4b44a724ae47cc5f1fedc4d0d8d7e33e277040ae
Festus Kiprop <fyangani@gmail.com>
2016-02-29 22:15:33 -0600

    Return

--- src/js/AnimalWrapper.jsx
+++ src/js/AnimalWrapper.jsx
@@ -10,6 +10,10 @@
     render: function() {
       var listItemClassList = this.props.active + " block Animal-wrapper p1 col col-12 mt1 mb1";
       var src = "http://www.chloechen.io/images/Animals/thumbnail/" + this.props.data.id + ".jpg";
+      return (
+        <li onClick={this.handleClick} className={listItemClassList} >
+        </li>
+      );
     }
   });
 
