877a6a8939663da5909f68551c1ff8726662c77d
Festus Kiprop <fyangani@gmail.com>
2016-02-29 22:14:20 -0600

    Get image source links and list Item class list, working on return

--- src/js/AnimalWrapper.jsx
+++ src/js/AnimalWrapper.jsx
@@ -8,6 +8,8 @@
     },
 
     render: function() {
+      var listItemClassList = this.props.active + " block Animal-wrapper p1 col col-12 mt1 mb1";
+      var src = "http://www.chloechen.io/images/Animals/thumbnail/" + this.props.data.id + ".jpg";
     }
   });
 
