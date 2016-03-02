d55d76e95d129dc37a71efaae102e8b679769eb5
Festus Kiprop <fyangani@gmail.com>
2016-03-02 15:07:47 -0600

    AnimalAttribute item to create html tags for rendering

--- src/js/AnimalsAttribute.jsx
+++ src/js/AnimalsAttribute.jsx
@@ -1,5 +1,10 @@
   var AnimalAttribute = React.createClass({
 
+
+  var AnimalAttributeItem = React.createClass({
+  render: function() {
+    return (<li><a href={this.props.link}>Image {this.props.index}</a> &copy; <a href={this.props.bio}>{this.props.author}</a></li>);
+  }
 });
 
 module.exports = AnimalAttribute;
