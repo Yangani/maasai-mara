7149b59acb1ecd07e93f51176bcc04f38fc0b17a
Festus Kiprop <fyangani@gmail.com>
2016-03-03 21:46:54 -0600

    Setting up chart defaults for scale

--- src/js/AnimalThreats.jsx
+++ src/js/AnimalThreats.jsx
@@ -2,3 +2,8 @@
 var Chart = require("chart.js");
 var RadarChart = require("react-chartjs").Radar;
 // To change other default settings, refer to : http://www.chartjs.org/docs/#getting-started-global-chart-configuration
+
+Chart.defaults.global.scaleOverride=true;
+Chart.defaults.global.scaleSteps=1;
+Chart.defaults.global.scaleStepWidth=10;
+Chart.defaults.global.scaleStartValue=0;
