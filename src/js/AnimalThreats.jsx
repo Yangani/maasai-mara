66748a7f1f378ed35d451a12ba5c41483cf9226e
Festus Kiprop <fyangani@gmail.com>
2016-03-03 21:45:12 -0600

    Animal threats chart, using chart.js

---
+++ src/js/AnimalThreats.jsx
@@ -0,0 +1,4 @@
+'use strict';
+var Chart = require("chart.js");
+var RadarChart = require("react-chartjs").Radar;
+// To change other default settings, refer to : http://www.chartjs.org/docs/#getting-started-global-chart-configuration
