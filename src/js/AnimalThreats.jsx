84ea79529486d45c27fac18d75f606be64d3cd20
Festus Kiprop <fyangani@gmail.com>
2016-03-03 21:55:26 -0600

    More chart options for point

--- src/js/AnimalThreats.jsx
+++ src/js/AnimalThreats.jsx
@@ -17,6 +17,21 @@ var chartOptions ={
     angleLineColor : "rgba(255,255,255,.3)",
     scaleLineColor: "rgba(255,255,255,.4)",
     angleLineWidth : 1,
-    responsive: true
+    responsive: true,
+    pointLabelFontFamily : "'Viga', sans-serif",
+    pointLabelFontStyle : "normal",
+    pointLabelFontSize : 10,
+    pointLabelFontColor : "#bdc3c7",
+    pointDot : true,
+    pointDotRadius : 3,
+    pointDotStrokeWidth : 1,
+    pointHitDetectionRadius : 20,
+    datasetStroke : true,
+    datasetStrokeWidth : 2,
+    datasetFill : true,
+    tooltipTemplate:"<%=label%>",
+    tooltipFillColor: "rgba(0,0,0,0.8)",
+    tooltipFontFamily: "'Viga', sans-serif",
+    legendTemplate : ""
 };
 
