e67b08ea446ab8e22ea829b76150da0ed7345e61
Festus Kiprop <fyangani@gmail.com>
2016-03-04 20:22:02 -0600

    Render function for animal status

--- src/js/AnimalStatus.jsx
+++ src/js/AnimalStatus.jsx
@@ -1,5 +1,11 @@
 'use strict';
   var AnimalStatus = React.createClass({
+    render: function(){
+      function transform(str){
+        var strNew = str.replace(/\s/g, "-");
+        return strNew;
+      }
+    }
   });
 
 module.exports = AnimalStatus;
