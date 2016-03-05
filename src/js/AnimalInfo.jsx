0b1ecc3c3e2bb4a3de2cd22c7cb3f3e31c1db1a2
Festus Kiprop <fyangani@gmail.com>
2016-03-05 15:12:09 -0600

    handle close function and animal info module function for export

--- src/js/AnimalInfo.jsx
+++ src/js/AnimalInfo.jsx
@@ -6,3 +6,12 @@ var AnimalStatus = require('./AnimalStatus.jsx');
 var AnimalImage = require('./AnimalImage.jsx');
 var AnimalAttribute = require('./AnimalAttribute.jsx');
 
+
+var AnimalInfo = React.createClass({
+    handleClose: function(){
+      this.props.onClick(this);
+    },
+   
+  });
+
+module.exports = AnimalInfo;
