0b9de77c0a4d558214ac1ea11daea68fe524e408
Festus Kiprop <fyangani@gmail.com>
2016-03-05 15:10:58 -0600

    Require files for rendering

---
+++ src/js/AnimalInfo.jsx
@@ -0,0 +1,8 @@
+'use strict';
+var AnimalTitle = require('./AnimalTitle.jsx');
+var AnimalStats = require('./AnimalStats.jsx');
+var AnimalThreats = require('./AnimalThreats.jsx');
+var AnimalStatus = require('./AnimalStatus.jsx');
+var AnimalImage = require('./AnimalImage.jsx');
+var AnimalAttribute = require('./AnimalAttribute.jsx');
+
