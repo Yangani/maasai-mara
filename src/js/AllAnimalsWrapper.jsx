4445aa33858d1cb5db429a0dd1230453814080b4
Festus Kiprop <fyangani@gmail.com>
2016-03-07 14:26:46 -0600

    declaring required variables and exporting all animals wrapper module

--- src/js/AllAnimalsWrapper.jsx
+++ src/js/AllAnimalsWrapper.jsx
@@ -2,3 +2,10 @@
   var AnimalWrapper = require('./AnimalWrapper.jsx');
   var AnimalInfo = require('./AnimalInfo.jsx');
   var buttonStart = document.getElementById('start');
+  var activeEl, transitionEvent, animals, animalImg;
+
+  var AllAnimalsWrapper = React.createClass({
+    
+  });
+
+module.exports = AllAnimalsWrapper;
